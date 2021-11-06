a63eb9cbb97870b16bffe5925506924b8db3dfa6
0dayatday0 <93808061+0dayatday0@users.noreply.github.com>
2021-11-06 07:21:48 +0100

    Update mmap_driver.bat

--- easy-kernelmapper/mmap_driver.bat
+++ easy-kernelmapper/mmap_driver.bat
@@ -1,5 +1,5 @@
-xcopy /y "mapper_driver.sys" "C:\mapper_driver.sys"
-xcopy /y "mapped_driver.sys" "C:\driver.sys"
+echo F|xcopy /y "mapper_driver.sys" "C:\mapper_driver.sys"
+echo F|xcopy /y "mapped_driver.sys" "C:\driver.sys"
 sc create kernel_mapper binpath="C:\mapper_driver.sys" type=kernel
 sc start kernel_mapper
 del /f /q "C:\driver.sys"
